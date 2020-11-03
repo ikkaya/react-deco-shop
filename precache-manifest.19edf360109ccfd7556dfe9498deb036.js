9eee53740e78bb0a10ba23de920805818ca33cb3
iremkaya <iiremkayaa22@gmail.com>
2020-11-03 14:39:42 +0300

    Updates

---
+++ precache-manifest.19edf360109ccfd7556dfe9498deb036.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d404456b607a4754226b53f76dd726d",
+    "url": "/react-deco-shop/index.html"
+  },
+  {
+    "revision": "dbe094248836d4191a23",
+    "url": "/react-deco-shop/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "ec1abec8e100bd31df17",
+    "url": "/react-deco-shop/static/css/main.dda4b58e.chunk.css"
+  },
+  {
+    "revision": "dbe094248836d4191a23",
+    "url": "/react-deco-shop/static/js/2.3a0d126d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-deco-shop/static/js/2.3a0d126d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1abec8e100bd31df17",
+    "url": "/react-deco-shop/static/js/main.2d5cf705.chunk.js"
+  },
+  {
+    "revision": "faaf525f1c010d12a870",
+    "url": "/react-deco-shop/static/js/runtime-main.01a3132e.js"
+  },
+  {
+    "revision": "8c433678b393019fd1de34fffc832cbd",
+    "url": "/react-deco-shop/static/media/img1.8c433678.jpg"
+  },
+  {
+    "revision": "470be1f2fc9aaa9c4746b8a51328a58d",
+    "url": "/react-deco-shop/static/media/img10.470be1f2.jpg"
+  },
+  {
+    "revision": "f3867f6df6a2f216cf21d184766705f9",
+    "url": "/react-deco-shop/static/media/img12.f3867f6d.jpg"
+  },
+  {
+    "revision": "5b944430d893180acbdf1b985d92075a",
+    "url": "/react-deco-shop/static/media/img2.5b944430.jpg"
+  },
+  {
+    "revision": "2901c66464f50d74dc1d7d5fc93eca64",
+    "url": "/react-deco-shop/static/media/img3.2901c664.jpg"
+  },
+  {
+    "revision": "59c48f350d03aae091d0b34f35b7da01",
+    "url": "/react-deco-shop/static/media/img6.59c48f35.jpg"
+  },
+  {
+    "revision": "78d779fdbba54f15215257a109517bcf",
+    "url": "/react-deco-shop/static/media/img7.78d779fd.jpg"
+  },
+  {
+    "revision": "e9a341f9e945d733d0dd63b07d20989c",
+    "url": "/react-deco-shop/static/media/img8.e9a341f9.jpg"
+  },
+  {
+    "revision": "d0d4e90429b330d6044e7a3a998aecac",
+    "url": "/react-deco-shop/static/media/img9.d0d4e904.jpg"
+  },
+  {
+    "revision": "880d64f71b27ab2dae959cd1006a8af7",
+    "url": "/react-deco-shop/static/media/logohome.880d64f7.svg"
+  }
+]);
